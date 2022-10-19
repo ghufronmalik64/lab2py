00b41557c122d282ec1e1fa7321ee2cbc1ac83ca
ghufronmalik64@gmail.com <ghufronmalik64@gmail.com>
2022-10-20 00:35:15 +0700

    memindahkan file

---
+++ latihan3/main.py
@@ -0,0 +1,8 @@
+# Menghitung luas dan keliling lingkaran
+
+r = input("Masukkan jari-jari lingkaran:")
+pi = 3.14
+l = pi * int(r) * int(r)
+k = 2 * pi * int(r)
+print("Luas lingkaran:", 1)
+print("Keliling lingkaran:", k)
